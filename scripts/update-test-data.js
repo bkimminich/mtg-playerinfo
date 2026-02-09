cce757de9200e0ce6b44d8cf4d0c8b728c199c7e
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2026-02-09 13:09:51 +0100

    Add workflow to update test data weekly

---
+++ scripts/update-test-data.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+const { request } = require('../src/utils/httpClient')
+
+const targets = [
+  { id: '16215', url: 'https://unityleague.gg/player/16215/', file: 'unityLeague.html' },
+  { id: '3irvwtmk', url: 'https://mtgeloproject.net/profile/3irvwtmk', file: 'mtgElo.html' },
+  { id: 'k0shiii', url: 'https://melee.gg/Profile/Index/k0shiii', file: 'melee.html' },
+  { id: 'k0shiii', url: 'https://topdeck.gg/profile/@k0shiii', file: 'topdeck.html' }
+]
+
+async function updateTestData () {
+  for (const target of targets) {
+    console.log(`Fetching ${target.url}...`)
+    try {
+      const { data } = await request(target.url, { maxRedirects: 10 })
+      const filePath = path.join(__dirname, '..', 'test', 'data', target.file)
+      fs.writeFileSync(filePath, data)
+      console.log(`Updated ${filePath}`)
+    } catch (error) {
+      console.error(`Error updating ${target.file}:`, error.message)
+      process.exit(1)
+    }
+  }
+}
+
+updateTestData()
